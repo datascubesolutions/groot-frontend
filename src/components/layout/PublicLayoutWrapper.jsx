dc85929d73b5e4374aa96631117d3a5f3b15d9be
Datascube Solutions <datascubesolutions@gmail.com>
2026-02-15 19:12:39 +0530

    Login Page

--- src/components/layout/PublicLayoutWrapper.jsx
+++ src/components/layout/PublicLayoutWrapper.jsx
@@ -5,8 +5,9 @@ import { usePathname } from "next/navigation";
 export function PublicLayoutWrapper({ children }) {
   const pathname = usePathname();
   const isAdmin = pathname?.startsWith("/admin");
+  const isAuth = pathname?.startsWith("/auth");
 
-  if (isAdmin) {
+  if (isAdmin || isAuth) {
     return null;
   }
 
